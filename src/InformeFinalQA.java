a7496a4fbd7d7b1f08779ace833340d2d1b957cc
elisabethuceda <elisabethuceda@gmail.com>
2026-08-27 17:41:41 +0200

    Actualizar título del informe QA

--- src/InformeFinalQA.java
+++ src/InformeFinalQA.java
@@ -5,7 +5,7 @@ public class InformeFinalQA {
 
         HashMap<String, String> resultados = new HashMap<>();
 
-        System.out.println("===== INFORME FINAL QA =====");
+        System.out.println("===== INFORME FINAL QA - PRUEBA =====");
 
         resultados.put("Login correcto", "OK");
         resultados.put("Login incorrecto", "KO");
