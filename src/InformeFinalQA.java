e267ab6905a7939513677d59a2380f0ace18be3b
elisabethuceda <36116067+elisabethuceda@users.noreply.github.com>
2026-09-01 11:43:44 +0200

    Change output message in InformeFinalQA

--- src/InformeFinalQA.java
+++ src/InformeFinalQA.java
@@ -5,7 +5,7 @@ public class InformeFinalQA {
 
         HashMap<String, String> resultados = new HashMap<>();
 
-        System.out.println("===== INFORME FINAL QA - GITHUB =====");
+        System.out.println("===== INFORME FINAL QA - CAMBIO REMOTO =====");
 
         resultados.put("Login correcto", "OK");
         resultados.put("Login incorrecto", "KO");
